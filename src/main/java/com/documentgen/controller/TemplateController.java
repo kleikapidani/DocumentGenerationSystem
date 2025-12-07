ad53ad0d25e68a05671ce5f984b24795638c4d68
Klei <kleikapidani24@gmail.com>
2025-12-07 16:55:00 +0100

    creation of template controller

---
+++ src/main/java/com/documentgen/controller/TemplateController.java
@@ -0,0 +1,12 @@
+package com.documentgen.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/template")
+@RequiredArgsConstructor
+public class TemplateController {
+
+}

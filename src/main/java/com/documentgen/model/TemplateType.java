aa8f9a28d32bf88318d7f2f4bfe86b6d007c860a
Klei <kleikapidani24@gmail.com>
2025-12-07 16:48:43 +0100

    creation of entity TemplateType

---
+++ src/main/java/com/documentgen/model/TemplateType.java
@@ -0,0 +1,31 @@
+package com.documentgen.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "template_type")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TemplateType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToOne(mappedBy = "templateType", fetch = FetchType.LAZY)
+    private Template template;
+}

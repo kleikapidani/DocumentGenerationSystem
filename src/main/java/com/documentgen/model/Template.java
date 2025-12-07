7d7e68fd2e19e7bdce044349c443ee61a88944fb
Klei <kleikapidani24@gmail.com>
2025-12-07 16:48:33 +0100

    creation of entity Template

---
+++ src/main/java/com/documentgen/model/Template.java
@@ -0,0 +1,36 @@
+package com.documentgen.model;
+
+import com.documentgen.enums.Language;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "template")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Template {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @Enumerated(value = EnumType.STRING)
+    private Language language;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_template_type")
+    private TemplateType templateType;
+}

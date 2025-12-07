2d36bfa4014b463cc3725020d58279483068c0d5
Klei <kleikapidani24@gmail.com>
2025-12-07 17:38:38 +0100

    implemented endpoint for creating the template

--- src/main/java/com/documentgen/controller/TemplateController.java
+++ src/main/java/com/documentgen/controller/TemplateController.java
@@ -1,6 +1,12 @@
 package com.documentgen.controller;
 
+import com.documentgen.request.CreateTemplateRequest;
+import com.documentgen.response.CreateTemplateResponse;
+import com.documentgen.service.TemplateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TemplateController {
 
+    private final TemplateService templateService;
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<CreateTemplateResponse> createTemplate(@RequestBody CreateTemplateRequest request) {
+        CreateTemplateResponse response = templateService.createTemplate(request);
+        return ResponseEntity.ok(response);
+    }
 }

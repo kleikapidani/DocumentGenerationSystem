fb543c5d9274bb7e11460c49dee99d14fcd98fcb
Klei <kleikapidani24@gmail.com>
2025-12-08 00:40:45 +0100

    refactoring

--- src/main/java/com/documentgen/service/TemplateService.java
+++ src/main/java/com/documentgen/service/TemplateService.java
@@ -2,6 +2,8 @@ package com.documentgen.service;
 
 import com.documentgen.exception.TemplateLanguageNotFoundException;
 import com.documentgen.exception.TemplateTypeNotFoundException;
+import com.documentgen.factory.TemplateFactory;
+import com.documentgen.mapper.TemplateMapper;
 import com.documentgen.model.Template;
 import com.documentgen.model.TemplateLanguage;
 import com.documentgen.model.TemplateType;
@@ -9,9 +11,7 @@ import com.documentgen.repository.TemplateLanguageRepository;
 import com.documentgen.repository.TemplateRepository;
 import com.documentgen.repository.TemplateTypeRepository;
 import com.documentgen.request.CreateTemplateRequest;
-import com.documentgen.response.TemplateLanguageResponse;
 import com.documentgen.response.CreateTemplateResponse;
-import com.documentgen.response.TemplateTypeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +23,8 @@ public class TemplateService {
     private final TemplateRepository templateRepository;
     private final TemplateTypeRepository templateTypeRepository;
     private final TemplateLanguageRepository templateLanguageRepository;
+    private final TemplateMapper templateMapper;
+    private final TemplateFactory templateFactory;
 
     @Transactional
     public CreateTemplateResponse createTemplate(CreateTemplateRequest request) {
@@ -33,34 +35,10 @@ public class TemplateService {
         TemplateLanguage templateLanguage = templateLanguageRepository.findById(request.getIdTemplateLanguage())
                 .orElseThrow(() -> new TemplateLanguageNotFoundException("Template language not found"));
 
-        Template newTemplate = new Template();
-        newTemplate.setName(request.getName());
-        newTemplate.setDescription(request.getDescription());
-        newTemplate.setTemplateLanguage(templateLanguage);
-        newTemplate.setTemplateType(templateType);
+        Template newTemplate = templateFactory.convertToTemplateEntity(request, templateLanguage, templateType);
 
         Template addedTemplate = templateRepository.save(newTemplate);
 
-        return CreateTemplateResponse.builder()
-                .id(addedTemplate.getId())
-                .name(addedTemplate.getName())
-                .description(addedTemplate.getDescription())
-                .createdAt(addedTemplate.getCreatedAt())
-                .updatedAt(addedTemplate.getUpdatedAt())
-                .templateTypeResponse(
-                        TemplateTypeResponse.builder()
-                                .id(addedTemplate.getTemplateType().getId())
-                                .name(addedTemplate.getTemplateType().getName())
-                                .description(addedTemplate.getTemplateType().getDescription())
-                                .build()
-                )
-                .templateLanguageResponse(
-                        TemplateLanguageResponse.builder()
-                                .id(addedTemplate.getTemplateLanguage().getId())
-                                .language(addedTemplate.getTemplateLanguage().getLanguage())
-                                .description(addedTemplate.getTemplateLanguage().getDescription())
-                                .build()
-                )
-                .build();
+        return templateMapper.convertToCreateTemplateResponse(addedTemplate);
     }
 }

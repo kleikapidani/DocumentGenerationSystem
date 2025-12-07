16b4a520fb2af8c09b23401c5e6d2f8f97df31b6
Klei <kleikapidani24@gmail.com>
2025-12-07 15:31:56 +0100

    First configuration project

---
+++ src/main/java/com/documentgen/DocumentGenerationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.documentgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentGenerationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentGenerationSystemApplication.class, args);
+	}
+
+}

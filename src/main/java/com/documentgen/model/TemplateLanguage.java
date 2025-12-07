b380d84961467f9c583ec3ac698cdc7c73790482
Klei <kleikapidani24@gmail.com>
2025-12-07 19:22:42 +0100

    creation of entity template language

---
+++ src/main/java/com/documentgen/model/TemplateLanguage.java
@@ -0,0 +1,36 @@
+package com.documentgen.model;
+
+import com.documentgen.enums.Language;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "template_language")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TemplateLanguage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(value = EnumType.STRING)
+    private Language language;
+
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "templateLanguage", fetch = FetchType.LAZY)
+    private List<Template> templates = new ArrayList<>();
+}

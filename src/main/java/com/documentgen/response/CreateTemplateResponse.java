51c5bed8dbc7f65ab5017ed7fc09dcebb9955249
Klei <kleikapidani24@gmail.com>
2025-12-08 15:44:04 +0100

    removed from response template, attributes createdAt and updatedAt

--- src/main/java/com/documentgen/response/CreateTemplateResponse.java
+++ src/main/java/com/documentgen/response/CreateTemplateResponse.java
@@ -5,8 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,8 +13,6 @@ public class CreateTemplateResponse {
     private Long id;
     private String name;
     private String description;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
     private TemplateTypeResponse templateTypeResponse;
     private TemplateLanguageResponse templateLanguageResponse;
 }

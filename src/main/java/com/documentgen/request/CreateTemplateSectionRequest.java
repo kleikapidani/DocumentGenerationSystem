067479f3d77bf445f9c92d7f6b6a0fcab40e6661
Klei <kleikapidani24@gmail.com>
2025-12-08 21:29:21 +0100

    added template section request to associate a new section to a specific template

---
+++ src/main/java/com/documentgen/request/CreateTemplateSectionRequest.java
@@ -0,0 +1,15 @@
+package com.documentgen.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateTemplateSectionRequest {
+    private String name;
+    private String description;
+    private Integer orderIndex;
+    private Long idTemplate;
+}

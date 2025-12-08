ad4683c9fccea978d213dee65578981f827a5c9f
Klei <kleikapidani24@gmail.com>
2025-12-08 21:43:44 +0100

    created class exception for template, in case template is not found in database

---
+++ src/main/java/com/documentgen/exception/TemplateNotFoundException.java
@@ -0,0 +1,7 @@
+package com.documentgen.exception;
+
+public class TemplateNotFoundException extends RuntimeException {
+    public TemplateNotFoundException(String message) {
+        super(message);
+    }
+}

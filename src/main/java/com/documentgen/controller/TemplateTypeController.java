993b586d4f8d22e980f50dc9afb0c5faa499da5e
Klei <kleikapidani24@gmail.com>
2025-12-08 01:05:56 +0100

    added endpoint and implementation service to insert template type

--- src/main/java/com/documentgen/controller/TemplateTypeController.java
+++ src/main/java/com/documentgen/controller/TemplateTypeController.java
@@ -1,13 +1,12 @@
 package com.documentgen.controller;
 
+import com.documentgen.request.CreateTemplateTypeRequest;
 import com.documentgen.response.TemplateTypeResponse;
 import com.documentgen.service.TemplateTypeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,11 @@ public class TemplateTypeController {
     public ResponseEntity<List<TemplateTypeResponse>> extractTemplateTypeList() {
         List<TemplateTypeResponse> templateTypeResponseList = templateTypeService.extractTemplateTypeList();
         return ResponseEntity.ok(templateTypeResponseList);
+    }
+
+    @PostMapping
+    public ResponseEntity<TemplateTypeResponse> createTemplateType(CreateTemplateTypeRequest request) {
+        TemplateTypeResponse templateTypeResponse = templateTypeService.createTemplateType(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(templateTypeResponse);
     }
 }
--- src/main/java/com/documentgen/service/TemplateTypeService.java
+++ src/main/java/com/documentgen/service/TemplateTypeService.java
@@ -1,7 +1,10 @@
 package com.documentgen.service;
 
+import com.documentgen.factory.TemplateTypeFactory;
 import com.documentgen.mapper.TemplateTypeMapper;
+import com.documentgen.model.TemplateType;
 import com.documentgen.repository.TemplateTypeRepository;
+import com.documentgen.request.CreateTemplateTypeRequest;
 import com.documentgen.response.TemplateTypeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,11 +17,22 @@ public class TemplateTypeService {
 
     private final TemplateTypeRepository templateTypeRepository;
     private final TemplateTypeMapper templateTypeMapper;
+    private final TemplateTypeFactory templateTypeFactory;
 
     public List<TemplateTypeResponse> extractTemplateTypeList() {
+
         return templateTypeRepository.findAll()
                 .stream()
                 .map(templateTypeMapper::convertToTemplateTypeResponse)
                 .toList();
+    }
+
+    public TemplateTypeResponse createTemplateType(CreateTemplateTypeRequest request) {
+
+        TemplateType templateType = templateTypeFactory.convertToTemplateTypeEntity(request);
+
+        TemplateType addedTemplateType = templateTypeRepository.save(templateType);
+
+        return templateTypeMapper.convertToTemplateTypeResponse(addedTemplateType);
     }
 }
